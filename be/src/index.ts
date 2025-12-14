30a85c5dad548dedf8939dd33fcf9d51c39c8d5f
sanke08 <sanketgawande08@gmail.com>
2025-12-14 22:52:40 +0530

    DB schema setup

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,8 +1,6 @@
 import app from "./app.js";
 
-// "dev": "tsc -b && node dist/index.js",
 const PORT = 3000;
-// import app from "./app";
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}\n http://localhost:${PORT}`);
